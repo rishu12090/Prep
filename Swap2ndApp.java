3e12b95bbf038196af003cfb57e53834f9cb37c4
rishu 12090 <rishu12090@gmail.com>
2025-03-11 17:14:38 +0530

    Approach 2nd of Swap

---
+++ Swap2ndApp.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Swap2ndApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Value of a = ");
+        int a = sc.nextInt();
+        System.out.println("Enter Value of b = ");
+        int b = sc.nextInt();
+        a += b;
+        b = a-b;
+        a = a-b;
+
+        System.out.println("Value of after swap A = "+a);
+        System.out.println("Value of afetr swap B = "+b);
+    }
+}
